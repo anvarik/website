cf983960472889e5a79ab10882352a5e97818aa3
enverbahar <bahar.enver@gmail.com>
2014-07-16 00:07:47 +0200

    heroku config

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ var app = express();
 
 app.use('/', express.static(__dirname + '/main'));
 
-console.log('listening on port 8088');
-app.listen(8081);
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function () {
+    console.log("Listening on " + port);
+});
